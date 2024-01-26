dda52878f26ba011e354a94d95b3592dcfb632b0
Srujan-github <101975044+Srujan-github@users.noreply.github.com>
2024-01-26 15:00:56 +0530

    service is added

---
+++ app/src/main/java/com/example/broadcast/service/AccessibiltyServiceClass.kt
@@ -0,0 +1,14 @@
+package com.example.broadcast.service
+
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
+class AccessibiltyServiceClass : AccessibilityService() {
+    override fun onAccessibilityEvent(p0: AccessibilityEvent?) {
+
+    }
+
+    override fun onInterrupt() {
+
+    }
+}
--- app/src/main/java/com/example/broadcast/ui/MainActivity.kt
+++ app/src/main/java/com/example/broadcast/ui/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.broadcast.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.broadcast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding= ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-
+        setContentView(binding.root)
     }
 }
